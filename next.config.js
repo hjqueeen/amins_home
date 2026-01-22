bc70dae46bfbe9fd57884371eca33271a3ec8f8a
Hyejin Kim <hjqueeen@gmail.com>
2026-01-22 11:32:50 +0100

    Build page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repositoryName = process.env.GITHUB_REPOSITORY_NAME || 'amins_home'
+
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
+  basePath: isProd ? `/${repositoryName}` : '',
+  assetPrefix: isProd ? `/${repositoryName}` : '',
+  images: {
+    unoptimized: true, // GitHub Pages는 이미지 최적화를 지원하지 않음
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
